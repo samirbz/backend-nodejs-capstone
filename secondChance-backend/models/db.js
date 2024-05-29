dd06c67ba9ea86989ff204ff03b4b94cd2589b03
samir <febsec14@gmail.com>
2024-05-29 13:24:50 +0545

    module 2 create api for secondChanceItems Service

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -1,35 +1,33 @@
 // db.js
 require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
 // MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
+const url = process.env.MONGO_URL;
+const dbName = process.env.MONGO_DB;
 
 let dbInstance = null;
-const dbName = `${process.env.MONGO_DB}`;
 
 async function connectToDatabase() {
     if (dbInstance) {
-        return dbInstance
-    };
-
-    const client = new MongoClient(url);
-
-    // Task 1: Connect to MongoDB
-    try {
-        await client.connect();
-        dbInstance = client.db(dbName)
         return dbInstance;
-    } catch (error) {
-        console.error("Failed to connect to the database", error)
-        throw error;
     }
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+    try {
+        const client = new MongoClient(url);
 
-    // Task 3: Return database instance
-    // {{insert code}}
+        // Connect to MongoDB
+        await client.connect();
+
+        // Connect to database and store in variable dbInstance
+        dbInstance = client.db(dbName);
+
+        // Return database instance
+        return dbInstance;
+    } catch (error) {
+        console.error('Error connecting to database:', error);
+        throw error;
+    }
 }
 
 module.exports = connectToDatabase;
